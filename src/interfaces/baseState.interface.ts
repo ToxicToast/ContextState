68c0b2d3482378b14d16133f33d4298fcb834910
Thomas Kortyka <info@toxictoast.de>
2021-04-09 15:17:39 +0200

    change appstate

--- src/interfaces/baseState.interface.ts
+++ src/interfaces/baseState.interface.ts
@@ -1,5 +1,3 @@
-interface IAppState<T> {}
-
-export function InitializeAppState<T extends object>(initAppState: IAppState<T>): IAppState<T> {
+export function InitializeAppState<T extends object>(initAppState: T): T {
 	return initAppState;
 }
